12d82921117ed7a750fdf2962ee2fbb5e8d7e8c1
sweetysutar <sweetysutar1702@gmail.com>
2025-05-29 16:45:28 +0530

    Initial commit with React build and JSON data
    Move json-server to dependencies

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import JobListings from '../components/JobListings';
+
+const JobsPage = () => {
+  return (
+    <section className='bg-blue-50c px-4 py-6'>
+      <JobListings />
+    </section>
+  )
+}
+
+export default JobsPage
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+// import { rewriter } from 'json-server'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+      target: 'http://localhost:8000',
+      changeOrigin: 'true',
+      rewrite: (path) => path.replace(/^\/api/, ''),
+    }
+  },
+    hmr: {
+      overlay: false
+    }
+  }
+})
